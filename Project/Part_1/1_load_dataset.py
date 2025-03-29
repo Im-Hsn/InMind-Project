5284695ccfba2d45ce7e2b6ebbe6332fb0965756
Hassan <89822402+Im-Hsn@users.noreply.github.com>
2025-03-29 17:52:33 +0200

    Part 1: 1- Loading Dataset
    Added the project files (dataset/labels) and completes the first step which is loading the dataset using "Dataloader".

---
+++ Project/Part_1/1_load_dataset.py
@@ -0,0 +1,28 @@
+import os
+import json
+from torch.utils.data import Dataset, DataLoader
+from PIL import Image
+
+class BMWObjectDataset(Dataset):
+    def __init__(self, image_dir, label_dir):
+        self.image_dir = image_dir
+        self.label_dir = label_dir
+        self.image_files = sorted([f for f in os.listdir(image_dir) if f.endswith(".png")])
+
+    def __len__(self):
+        return len(self.image_files)
+
+    def __getitem__(self, idx):
+        image_path = os.path.join(self.image_dir, self.image_files[idx])
+        label_path = os.path.join(self.label_dir, self.image_files[idx].replace(".png", ".json"))
+        
+        image = Image.open(image_path).convert("RGB")
+        with open(label_path, "r") as f:
+            labels = json.load(f)
+        
+        return image, labels
+
+image_dir = "../data/images"
+label_dir = "../data/labels"
+dataset = BMWObjectDataset(image_dir, label_dir)
+dataloader = DataLoader(dataset, batch_size=4, shuffle=True)
